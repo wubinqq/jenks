59170d5164fbda4fd7c012307fb2de641916d3ef
wubin <229153988@qq.com>
2018-01-01 21:43:07 +0800

    first commit

---
+++ src/main/java/com/gen/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.gen;
+
+/**
+ * @date 2018/1/1 13:11
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World !!!");
+    }
+}
